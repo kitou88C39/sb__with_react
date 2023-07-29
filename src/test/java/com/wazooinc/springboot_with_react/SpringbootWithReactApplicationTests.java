8c43a237a4e3e534849696bede7b6f235f0bde3a
kitou88C39 <kanfutrooper@icloud.com>
2023-07-29 16:16:20 +0900

    first commit

---
+++ src/test/java/com/wazooinc/springboot_with_react/SpringbootWithReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wazooinc.springboot_with_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
